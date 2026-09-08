b4855de86db305140eee039694e563c7b72c6b9e
Dmytro Honcharenko <dimeeentor@gmail.com>
2026-09-08 22:06:02 +0200

    feat: add responsive compact layout with footer and room tools

---
+++ frontend/src/ui/Footer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import RoomTools from "./RoomTools.svelte"
+</script>
+
+<footer class="footer">
+  <div class="shortcuts">
+    <span><kbd>/</kbd> link field</span>
+    <span><kbd>Space</kbd> play / pause</span>
+    <span><kbd>&larr;</kbd><kbd>&rarr;</kbd> seek 5s</span>
+    <span><kbd>F</kbd> fullscreen</span>
+    <span><kbd>Esc</kbd> blur</span>
+  </div>
+  <RoomTools />
+</footer>
